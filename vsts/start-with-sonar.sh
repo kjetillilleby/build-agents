c53268ad4084ab190c35997325a93c3ba225bb8b
Kjetil Lilleby <KLilleby@slb.com>
2018-04-05 09:18:52 +0200

    restart

--- vsts/start-with-sonar.sh
+++ vsts/start-with-sonar.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#add certificates to java keystore
 keytool -importcert -file /usr/local/share/ca-certificates/sonarqube-test.cer -keystore $JVM_KEYSTORE -alias "SonarQube Test" -storepass changeit -noprompt
 keytool -importcert -file /usr/local/share/ca-certificates/sonarqube-dev.cer -keystore $JVM_KEYSTORE -alias "SonarQube Dev" -storepass changeit -noprompt
+
+#start agent
 ./start.sh
